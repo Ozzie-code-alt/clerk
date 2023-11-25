d4b223fbb1509551501ab7cd9703689e06bc09c5
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-11-25 21:06:10 +0800

    Sign up and Signin in house clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
 <div>
-  
+ <p className='text-2xl font-bold mb-5'>Welcome</p>
+ <p className='mb-5'> Demo website Lmao</p>
 </div>
   )
 }
